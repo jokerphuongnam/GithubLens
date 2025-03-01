99e398d652ed591695d278734089cef2140e2b82
Phạm Phương Nam <phuongnamp99@gmail.com>
2025-03-01 16:48:15 +0700

    init git

---
+++ Apps/GihubLens/GihubLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  GihubLensApp.swift
+//  GihubLens
+//
+//  Created by Phạm Phương Nam on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GihubLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/GihubLensTests/GihubLensTests.swift
@@ -0,0 +1,17 @@
+//
+//  GihubLensTests.swift
+//  GihubLensTests
+//
+//  Created by Phạm Phương Nam on 3/1/25.
+//
+
+import Testing
+@testable import GihubLens
+
+struct GihubLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
