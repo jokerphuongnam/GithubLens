675dbd391f2fb02d1053b2d91a444ab2def316b8
Phạm Phương Nam <phuongnamp99@gmail.com>
2025-03-04 18:24:40 +0700

    Impelement loadmore github users

--- Apps/GihubLens/Database/Domains/LoadMoreState.swift
+++ Apps/GihubLens/Database/Domains/LoadMoreState.swift
@@ -1,11 +1,50 @@
-struct LoadMoreState<T>: Hashable where T: Hashable {
-    var resource: Resource<T>
-    var loadMore: PrepareLoadMore
+struct LoadMoreResource<T>: Hashable where T: Hashable {
+    var resource: Resource<Array<T>>
+    var prepareLoadMore: PrepareLoadMore
+    var loadMoreState: LoadMoreState?
     var page: Int
+    
+    mutating func increasePage() {
+        page += 1
+    }
+    
+    mutating func loadMore(moreData: Array<T>) {
+        self.resource = resource.loadMore(moreData: moreData)
+    }
 }
 
 enum PrepareLoadMore: Hashable {
     case canLoadMore
     case cannotLoadMore
     case success
+}
+
+enum LoadMoreState: Hashable {
+    case loading
+    case error(error: Error)
+}
+
+extension LoadMoreState {
+    static func ==(lhs: LoadMoreState, rhs: LoadMoreState) -> Bool {
+        switch (lhs, rhs) {
+        case (.loading, .loading):
+            return true
+        case let (.error(lhsError), .error(rhsError)):
+            // Using localizedDescription as a proxy for equality.
+            return lhsError.localizedDescription == rhsError.localizedDescription
+        default:
+            return false
+        }
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .loading:
+            hasher.combine(0)
+        case .error(let error):
+            hasher.combine(1)
+            // Again, using localizedDescription to generate a hash value.
+            hasher.combine(error.localizedDescription)
+        }
+    }
 }
--- Apps/GihubLens/UIs/Screens/GithubUsers/GihubUsersViewModel.swift
+++ Apps/GihubLens/UIs/Screens/GithubUsers/GihubUsersViewModel.swift
@@ -4,7 +4,11 @@ import GithubLensNetworks
 @Observable
 final class GihubUsersViewModel {
     private let useCase: GithubUsersUserCase
-    var usersState: LoadMoreState<[GithubUser]> = .init(resource: .loading, loadMore: .cannotLoadMore, page: 0)
+    var usersState: LoadMoreResource<GithubUser> = .init(
+        resource: .loading,
+        prepareLoadMore: .canLoadMore,
+        page: 0
+    )
     
     init(useCase: GithubUsersUserCase) {
         self.useCase = useCase
@@ -12,34 +16,78 @@ final class GihubUsersViewModel {
     
     func loadGitHubUsers() {
         usersState.resource = .loading
-        Task(priority: .utility) {
-            await refreshGitHubUsers()
+        Task(priority: .utility) { [weak self] in
+            guard let self else { return }
+            await self.refreshGitHubUsers()
         }
     }
     
     func refreshGitHubUsers() async {
         do {
             let users = try await useCase.fetchGithubUser(page: usersState.page)
-            Task { @MainActor in
-                usersState.resource = .success(data: users)
+            Task { @MainActor [weak self] in
+                guard let self else { return }
+                self.usersState.resource = .success(data: users)
+                self.usersState.increasePage()
             }
         } catch {
-            Task { @MainActor in
-                usersState.resource = .failure(error: error)
+            Task { @MainActor [weak self] in
+                guard let self else { return }
+                self.usersState.resource = .failure(error: error)
             }
         }
     }
     
     func loadMore() {
-        Task(priority: .utility) {
-            guard usersState.loadMore == .cannotLoadMore else { return }
-            guard case .success(data: _) = usersState.resource else { return }
+        Task(priority: .utility) { [weak self] in
+            guard let self else { return }
+            guard self.usersState.prepareLoadMore == .canLoadMore else { return }
+            guard case .success(data: _) = self.usersState.resource else { return }
+            Task { @MainActor [weak self] in
+                guard let self else { return }
+                self.usersState.loadMoreState = .loading
+                self.usersState.prepareLoadMore = .cannotLoadMore
+            }
             
             do {
-                
+                let users = try await self.useCase.fetchGithubUser(page: self.usersState.page)
+                if users.isEmpty {
+                    Task { @MainActor [weak self] in
+                        guard let self else { return }
+                        self.usersState.prepareLoadMore = .success
+                        self.usersState.loadMoreState = nil
+                        
+                    }
+                } else {
+                    let newState = self.usersState.resource.loadMore(moreData: users)
+                    if let _ = newState.data {
+                        Task { @MainActor [weak self] in
+                            guard let self else { return }
+                            self.usersState.prepareLoadMore = .canLoadMore
+                            self.usersState.increasePage()
+                        }
+                    }
+                    Task { @MainActor [weak self] in
+                        guard let self else { return }
+                        self.usersState.resource = newState
+                        self.usersState.loadMoreState = nil
+                    }
+                }
             } catch {
-                
+                Task { @MainActor [weak self] in
+                    guard let self else { return }
+                    self.usersState.loadMoreState = .error(error: error)
+                }
             }
         }
+    }
+    
+    func resetLoadMore() {
+        Task { @MainActor [weak self] in
+            guard let self else { return }
+            self.usersState.prepareLoadMore = .canLoadMore
+            self.usersState.loadMoreState = .loading
+        }
+        loadMore()
     }
 }
